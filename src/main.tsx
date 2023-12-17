d797b3be9e5d539475874870ff0cd5bfc091e435
Abir <abir4dev@gmail.com>
2023-12-17 12:24:35 +0600

    Filter games by genre

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
+import App from "./App";
 import "./index.css";
-import theme from "./theme.ts";
+import theme from "./theme";
 import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
