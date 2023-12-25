078a1082fefa3cd4be766ed50d04c2c781f19189
Abir <abir4dev@gmail.com>
2023-12-26 00:01:44 +0600

    Refactor -api key

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -7,7 +7,7 @@ export interface FetchResponse<T> {
 const axiosInstance = axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
-    key: "85d299421bac4c5eab2d082f22bedf25",
+    key: import.meta.env.VITE_API_KEY,
   },
 });
 
